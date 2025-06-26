66ee2efc80dd74a9cd32ca9247735339c3bbba26
kaktu5 <kpiatkowski@protonmail.com>
2025-06-26 20:10:54 +0200

    ignore #[hot] proc macro
    ignore #[hot] proc macro
    
    ignore #[hot] proc macro
    
    ignore #[hot] proc macro

--- config/lsp/languages.nix
+++ config/lsp/languages.nix
@@ -8,7 +8,22 @@ _: {
     nix = {enable = true;} // {lsp.server = "nixd";};
     nu.enable = true;
     ocaml.enable = true;
-    rust = {enable = true;} // {crates.enable = true;};
+    rust = {
+      enable = true;
+      crates.enable = true;
+      lsp.opts = ''
+        ["rust-analyzer"] = {
+          procMacro = {
+            ignored = {
+              bevy_simple_subsecond_system_macros = { "hot" },
+            },
+            diagnostics = {
+              disabled = { "proc-macro-disabled" },
+            },
+          },
+        },
+      '';
+    };
     typst = {enable = true;} // {format.type = "typstyle";};
     wgsl.enable = true;
     zig.enable = true;

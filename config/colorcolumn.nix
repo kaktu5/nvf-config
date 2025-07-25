be27108dd5489a35274fe72892e95874abd0e53f
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-07-25 09:11:29 +0200

    fix

--- config/colorcolumn.nix
+++ config/colorcolumn.nix
@@ -11,13 +11,13 @@ in {
     visuals.indent-blankline = {
       enable = true;
       setupOpts = {
-        indent.highlight = "IndentBlankline";
+        # indent.highlight = "IndentBlankline";
         scope.enabled = false;
       };
     };
     highlight.ColorColumn.bg = colors.bg1;
-    luaConfigRC.IndentBlankline = entryBefore ["indent-blankline"] ''
-      vim.api.nvim_set_hl(0, "IndentBlankline", ${toLuaObject {fg = colors.bg1;}})
-    '';
+    # luaConfigRC.IndentBlankline = entryBefore ["indent-blankline"] ''
+    #   vim.api.nvim_set_hl(0, "IndentBlankline", ${toLuaObject {fg = colors.bg1;}})
+    # '';
   };
 }
